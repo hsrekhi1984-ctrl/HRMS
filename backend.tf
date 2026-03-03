522063eb0aa5a11c5acfc01b14b8040149c53215
hsrekhi1984-ctrl <hsrekhi1984@gmail.com>
2026-03-03 21:07:29 +0530

    Configure Azure backend for Terraform state

--- backend.tf
+++ backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "hrmsterraformstate123"
+    container_name       = "tfstate"
+    key                  = "dev/terraform.tfstate"
+  }
 }
